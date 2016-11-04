ec5c80afe21bbd22543d63204d3bc80110e97ea5
Cgumacal <chris.gumacal94@gmail.com>
2016-11-04 13:25:01 -0700

    full screen and portrait lock

--- app/src/main/java/com/example/ninaly/adventuregame453/StartScreen.java
+++ app/src/main/java/com/example/ninaly/adventuregame453/StartScreen.java
@@ -1,9 +1,10 @@
 package com.example.ninaly.adventuregame453;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StartScreen extends AppCompatActivity {
+public class StartScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
