8e89942d30f35be2a43dbe5d208107ddbff712f4
Cgumacal <chris.gumacal94@gmail.com>
2016-11-07 10:13:36 -0800

    Title Screen with place holder graphic
    created layout for title screen and put in placeholder graphics
    
    Created a drawable folder where we can place all the images

--- app/src/main/java/com/example/ninaly/adventuregame453/StartScreen.java
+++ app/src/main/java/com/example/ninaly/adventuregame453/StartScreen.java
@@ -3,6 +3,8 @@ package com.example.ninaly.adventuregame453;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class StartScreen extends Activity {
 
@@ -10,5 +12,18 @@ public class StartScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+    }
+
+    public void onNewGameClick(View view){
+        Toast.makeText(StartScreen.this,"New Game click", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onContinueClick(View view){
+        Toast.makeText(StartScreen.this,"Continue click", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onExitClick(View view){
+        System.exit(0);
     }
 }
