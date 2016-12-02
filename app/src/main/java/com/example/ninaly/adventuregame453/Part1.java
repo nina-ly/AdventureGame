413e4fce8cfc4a6304dbb8466f257be18e7bc2da
nina-ly <nina.ly10@gmail.com>
2016-12-02 11:06:45 -0800

    Added layout files

--- app/src/main/java/com/example/ninaly/adventuregame453/Part1.java
+++ app/src/main/java/com/example/ninaly/adventuregame453/Part1.java
@@ -2,17 +2,13 @@ package com.example.ninaly.adventuregame453;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.view.Gravity;
-
-import java.lang.reflect.Array;
-import java.util.Random;
 
 public class Part1 extends Activity {
 
@@ -22,29 +18,6 @@ public class Part1 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.part1_intro);
-
-        /*
-        swingButton = (Button) findViewById(R.id.swing);
-        askButton = (Button) findViewById(R.id.ask);
-
-
-
-        swingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.part1_take_swing);
-               // Toast toast = Toast.makeText(Part1.this,"Hey! What's that for?! I'm not harmful.",Toast.LENGTH_SHORT);
-               // toast.setGravity(Gravity.CENTER,0,50);
-               // toast.show();
-            }
-        });
-
-        askButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.part1_ask_fairy);
-            }
-        });      */
 
     }
 
@@ -113,7 +86,7 @@ public class Part1 extends Activity {
 
             // From part1_dark_path.xml
             case R.id.runAwayButton:
-                setContentView(R.layout.part1_run_away_from_goblin);
+                setContentView(R.layout.part1_sword);
                 break;
 
             // From part1_dark_path.xml
@@ -121,7 +94,106 @@ public class Part1 extends Activity {
                 setContentView(R.layout.part1_say_hello);
                 break;
 
+            // From part1_dark_path.xml
+            case R.id.attackButton:
+                setContentView(R.layout.part1_attack_goblin);
+                break;
 
+            // From part1_say_hello.xml
+            case R.id.attackGoblin:
+                setContentView(R.layout.part1_attack_goblin);
+                break;
+
+            // From part1_say_hello.xml
+            case R.id.leaveButton:
+                setContentView(R.layout.part1_sword);
+                break;
+
+            // From part1_say_hello.xml
+            case R.id.nodButton:
+                setContentView(R.layout.part1_nod_globin);
+                break;
+
+            // From part1_attack_globin.xml
+            case R.id.keepLookingButton:
+                setContentView(R.layout.part1_keep_looking);
+                break;
+
+            // From part1_attack_globin.xml
+            //add leave logic
+
+            // From part1_keep_looking.xml
+            case R.id.keepLookingButton2:
+                setContentView(R.layout.part1_keep_looking_again);
+                break;
+
+            // From part1_keep_looking_again.xml
+            case R.id.gameOver:
+                setContentView(R.layout.part1_gameover);
+                break;
+
+            // From part1_gameover.xml
+            case R.id.mainMenu:
+                setContentView(R.layout.activity_start_screen);
+                break;
+
+            case R.id.quitGame:
+                System.exit(0);
+                break;
+
+            //add part1_spanish_setting.xml
+
+            // From part1_goblin_help.xml
+            case R.id.swordHelpFromGoblin:
+                setContentView(R.layout.part1_sword);
+                break;
+
+            // From part1_sword.xml
+            case R.id.pullSword:
+                setContentView(R.layout.part1_pull_sword);
+                break;
+
+            // From part1_sword.xml
+            case R.id.readInscription:
+                setContentView(R.layout.part1_read_inscription);
+                break;
+
+            // From part1_sword.xml
+            case R.id.keepWalking:
+                setContentView(R.layout.part1_keep_walking);
+                break;
+
+            // From part1_pull_sword.xml
+            case R.id.readInscription1:
+                setContentView(R.layout.part1_read_inscription);
+                break;
+
+            // From part1_read_inscription.xml
+            case R.id.pullSword1:
+                setContentView(R.layout.part1_pull_sword2);
+                break;
+
+            // From part1_read_inscription.xml
+            case R.id.leaveInscription:
+                setContentView(R.layout.part1_leave_inscription);
+                break;
+
+            // From part1_keep_walking.xml
+            case R.id.part2Butt:
+                Intent part2 = new Intent(Part1.this, Cave.class);
+                startActivity(part2);
+                break;
+
+            // From part1_leave_insscription.xml
+            case R.id.goToPart2:
+                Intent partdos = new Intent(Part1.this, Cave.class);
+                startActivity(partdos);
+                break;
+
+            // From part1_pull_sword2.xml
+            case R.id.takeTheSword:
+                setContentView(R.layout.part1_leave_inscription);
+                break;
 
         } //end switch
     } //end onClick
@@ -166,6 +238,89 @@ public class Part1 extends Activity {
                     sword.show();
                 }
 
+                break;
+
+            case R.id.attackGoblinFairy:
+                Toast jumpHim = Toast.makeText(Part1.this, "I think I heard something... we should get out of here.", Toast.LENGTH_LONG);
+                jumpHim.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                jumpHim.show();
+                break;
+
+            case R.id.keepLookingFairy:
+                Toast kl = Toast.makeText(Part1.this, "Seriously.. we should leave now!", Toast.LENGTH_LONG);
+                kl.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                kl.show();
+                break;
+
+            case R.id.keepLookingFairy2:
+                Toast kla = Toast.makeText(Part1.this, "I told you we should've left! It's too late now.", Toast.LENGTH_LONG);
+                kla.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                kla.show();
+                break;
+
+            case R.id.leaveAfter:
+                Toast leave = Toast.makeText(Part1.this, "Oooo trying pulling the sword.", Toast.LENGTH_LONG);
+                leave.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                leave.show();
+                break;
+
+            case R.id.spanishFairy:
+                Toast spanish = Toast.makeText(Part1.this, "Is he speaking in Spanish? We need to learn spanish to understand. Change the language settings on your phone to Spanish to get the English translation. Pointing is rude so don't upset him.", Toast.LENGTH_LONG);
+                spanish.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                spanish.show();
+                break;
+
+            case R.id.angryGoblin:
+                Toast angry = Toast.makeText(Part1.this, "Uh oh, your pointing upsetted him. Do you still want to look around?", Toast.LENGTH_LONG);
+                angry.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                angry.show();
+                break;
+
+            case R.id.settingButton:
+                startActivityForResult(new Intent(Settings.ACTION_LOCALE_SETTINGS),0);
+                break;
+
+            case R.id.goblinHelp:
+                Toast help = Toast.makeText(Part1.this, "The goblin was very friendly. Let's follow the path.", Toast.LENGTH_LONG);
+                help.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                help.show();
+                break;
+
+            case R.id.swordFairy:
+                Toast sword = Toast.makeText(Part1.this, "Hmm.... trying pulling the sword?", Toast.LENGTH_LONG);
+                sword.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                sword.show();
+                break;
+
+            case R.id.pullSwordFairy:
+                Toast fairy = Toast.makeText(Part1.this, "Try reading the inscription.", Toast.LENGTH_LONG);
+                fairy.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                fairy.show();
+                break;
+
+            case R.id.readInscriptionFairy:
+                Toast rf = Toast.makeText(Part1.this, "Have you done anything bad lately? Maybe we should go.", Toast.LENGTH_LONG);
+                rf.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                rf.show();
+                break;
+
+            case R.id.walkAwayFairy:
+                Toast wa = Toast.makeText(Part1.this, "Yay!!!!", Toast.LENGTH_LONG);
+                wa.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                wa.show();
+                break;
+
+            case R.id.pullSwordFairy2:
+                Toast ps = Toast.makeText(Part1.this, "If you didn't attack the goblin or if you didn't shoo me away, leave with the sword." +
+                        "If you did attack the goblin or shoo me away, you are about to experience karma.", Toast.LENGTH_LONG);
+                ps.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                ps.show();
+                break;
+
+            case R.id.leaveFairy:
+                Toast leave1 = Toast.makeText(Part1.this, "I think we're almost home!", Toast.LENGTH_LONG);
+                leave1.setGravity(Gravity.TOP | Gravity.RIGHT, 0, 110);
+                leave1.show();
                 break;
 
         } //end switch
